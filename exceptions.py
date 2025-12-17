9faa65cc488a6dd7e1560e2794016bf2177de44c
Jazmin Horton <raverzv@gmail.com>
2025-12-17 09:20:34 -0700

    added 3 minimum exceptions
    we can add more if we feel like it

---
+++ exceptions.py
@@ -0,0 +1,17 @@
+class FulfillmentError(Exception):
+    """Base class for all exceptions in this project.
+    inheriting from should let us catch all exceptions
+    with one except block. (if we wanted to)
+    """
+    pass
+
+class OutofStockError(FulfillmentError):
+    """raised when a warehouse doesn't have enough stock 
+    to fulfill an order.
+    """
+    pass
+
+class InvalidOrderError(FulfillmentError):
+    """This is riased when an order asks for a product that isn't found in the catalog.""
+    """
+    pass
